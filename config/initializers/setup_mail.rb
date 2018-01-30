ea54291690849e3f111b0fb3fdcd22c26a53a7e8
Rubysdad <markandtabithamorales@gmail.com>
2018-01-30 19:50:10 +0000

    bug

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.smtp_settings = {
     :user_name =>      ENV['SPARKPOST_SMTP_USERNAME'],
     :password =>       ENV['SPARKPOST_SMTP_PASSWORD'],
     :authentication => :login,
-    :enable_starttls_auto => true
+    :enable_starttls_auto => true,
     :domain 'heavyladen.herokuapp.com'
 }
 ActionMailer::Base.delivery_method = :smtp
